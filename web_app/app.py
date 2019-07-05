6a933f074ebd786123bc8185dd67fb529bb37748
Robert Lin <robert.lin0397@gmail.com>
2019-07-04 22:56:26 -0400

    finished most of frontend work for the project

--- web_app/app.py
+++ web_app/app.py
@@ -1,11 +1,18 @@
-from flask import Flask, request, render_template
-
+from flask import Flask, request, render_template, redirect, url_for 
+from models import get_user_cron_tabs, add_user_cron, remove_user_cron
 
 app = Flask(__name__)
-
+weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
 
 def __init__(self):
     app.static_folder = 'static'
+
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    data = {'title': 'home',
+            'crons': get_user_cron_tabs()}
+    return render_template('home.html', **data)
 
 
 @app.route('/add_new_alarm', methods=['POST', 'GET'])
@@ -13,10 +20,37 @@ def add_new_alarm():
     data = {'title': 'add a new alarm'}
 
     if request.method == 'POST':
-        alarm_time = request.form['time']
-        return alarm_time
+        alarm_time = request.form.get('time')
+        
+        if not alarm_time:
+            return "Please return a response with alarm time."
+        
+        alarm_on_days = []
 
-    return render_template('home.html', **data)
+        for weekday in weekdays:
+            alarm_on = request.form.get(weekday)
+            if alarm_on:
+                alarm_on_days.append(weekday)
+
+        if len(alarm_on_days) == 0:
+            return "Please input a valid day(s) to have the alarm on."
+
+        add_user_cron(alarm_on_days, alarm_time)
+        return redirect(url_for('home'))
+        
+    else:
+        return render_template('add_new_alarm.html', **data)
+
+@app.route('/remove_alarm', methods=['POST'])
+def remove_alarm():
+    comment = request.form.get("comment")
+
+    if not comment:
+        return redirect(url_for('home'))
+
+    remove_user_cron(comment)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
+    app.run(debug=True, port=5000, host='192.168.1.142')
+
---
+++ web_app/models.py
@@ -0,0 +1,61 @@
+from crontab import CronTab, CronItem
+
+command_str = '.virtualenvs/smart_alarm/bin/python projects/smart_alarm/script.py'
+
+def _get_hour_minute(alarm_time: str):
+    ## gets the index of ':' for minute and hour for schedule
+    index = alarm_time.find(':')
+    minute = alarm_time[index + 1:]
+    hour = alarm_time[:index]
+    return int(hour), int(minute)
+
+
+def _get_alarm_times(item : CronItem) -> str:
+    return str(item.dow) + " " + str(item.hour) + " " + str(item.minute)
+
+
+def get_user_cron_tabs():
+    user_cron = CronTab(user=True)
+    return [job.comment for job in user_cron]
+
+def add_user_cron(days_on: list, alarm_time: str) -> None:
+    """
+    This function adds a new crontab for the alarm with schedule on
+    days in the list days_on. For example, ["Mon", "Thu"] means the
+    alarm script will run during Monday and Thursday. The alarm_time
+    is the time when the script will run
+    """
+
+    ## creates new crontab with alarm script command
+    user_cron = CronTab(user=True)
+    job = user_cron.new(command = command_str)
+    
+    ## setting the schedule
+    hour_val, minute_val = _get_hour_minute(alarm_time)
+    job.minute.on(hour_val)
+    job.hour.on(minute_val)
+    
+    ## setting the days of schedule
+    days_on = [day.upper() for day in days_on]
+    job.dow.on(*days_on)
+    
+    ## setting comment to use for display and delete id later
+    job.comment = str(job.dow) + " " + alarm_time
+
+    user_cron.write_to_user(user=True)
+
+
+def remove_user_cron(comment_str: str):
+    """
+    This function removes crontabs for the alarm with the comment
+    of this comment string passed in
+    """
+
+    ## gets the crontab
+    user_cron = CronTab(user=True)
+    user_cron.remove_all(comment=comment_str)
+    user_cron.write_to_user(user=True)
+
+
+if __name__ == '__main__':
+    add_user_cron(['mon', 'tue'], "12:00")
