bda340e99ad2446a2c784a0da5548a61933d7893
Robert Lin <robert.lin0397@gmail.com>
2019-07-13 15:47:29 -0400

    Finished smart alarm

--- script.py
+++ script.py
@@ -1,33 +1,48 @@
 from alarm import Alarm
-import RPi.GPIO as GPIO
 from time import sleep
+from lifxlan import LifxLAN
 import os
+
 
 def play_audio(file_name=None):
     if file_name == None:
         return
 
-    audio_cmd_line = "omxplayer " 
+    audio_cmd_line = "mplayer " 
     cmd_line = audio_cmd_line + file_name
     os.system(cmd_line)
 
 
+def set_state(bulb=None, state=None):
+    if not bulb or not state:
+        return
+
+    bulb.set_power(state)
+
+def init_bulb(num_lights=1):
+    lifx = LifxLAN(num_lights)
+    devices = lifx.get_lights()
+
+    # I have only one Lifx light currently, may change this code in the future
+    bulb = devices[0]
+    return bulb
+
 if __name__ =='__main__':
     dirname = os.path.dirname(__file__)
     path = os.path.join(dirname, 'SmartAlarm-01b89ee73267.json')
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(18, GPIO.OUT)
 
     try:
+        bulb = init_bulb()
+        set_state(bulb, "on")
         my_alarm = Alarm(path=path)
-        GPIO.output(18, GPIO.HIGH)
-        sleep(2)
-        my_alarm.take_a_picture()
-        still_in_bed = my_alarm.find_people_in_feed()
+        still_in_bed = True
+        while still_in_bed:
+            still_in_bed = my_alarm.find_people_in_feed()
         
-        if still_in_bed:
-            print("GET OUT OF BED RIGHT NOW!")
-            play_audio(os.getcwd() + "/wake_up_sounds/alarm_sound.mp3")
+            if still_in_bed:
+                print("GET OUT OF BED RIGHT NOW!")
+                play_audio(os.getcwd() + "/wake_up_sounds/alarm_sound.mp3")
+                sleep(30)
 
     except Exception as e:
         print(e)
@@ -36,7 +51,6 @@ if __name__ =='__main__':
 
     
     finally:
-        GPIO.output(18, GPIO.LOW)
-        GPIO.cleanup()
-
+        set_state(bulb, "off")
+        print("Script finished")
 
